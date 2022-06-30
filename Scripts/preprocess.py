e51d27d2a552ed36cb32a951eda4ef58f10706e0
Davidelvis <komboelvis08@gmail.com>
2022-06-30 22:33:31 +0300

    ML scientist challenge

---
+++ Scripts/preprocess.py
@@ -0,0 +1,81 @@
+import argparse
+import os
+import pickle
+
+import pandas as pd
+from sklearn.feature_extraction import DictVectorizer
+
+
+def dump_pickle(obj, filename):
+    with open(filename, "wb") as f_out:
+        return pickle.dump(obj, f_out)
+
+
+def read_dataframe(filename: str):
+    df = pd.read_pickle(filename)
+
+    categorical = ['dob','gender','region','district','im_date','successful','reason_unsuccesful']
+    df[categorical] = df[categorical].astype(str)
+
+    return df
+
+
+def preprocess(df: pd.DataFrame, dv: DictVectorizer, fit_dv: bool = False):
+    categorical = ['dob','gender','region','district','im_date','successful','reason_unsuccesful']
+    numerical = ['fac_id','pat_id','DTP','OPV','lat','long']
+    dicts = df[categorical + numerical].to_dict(orient='records')
+    if fit_dv:
+        X = dv.fit_transform(dicts)
+    else:
+        X = dv.transform(dicts)
+    return X, dv
+
+
+def run(raw_data_path: str, dest_path: str, dataset: str = "green"):
+    # load pickle files
+    df_train = read_dataframe(
+        os.path.join(raw_data_path, f"{dataset}_training_data")
+    )
+    df_valid = read_dataframe(
+        os.path.join(raw_data_path, f"{dataset}_validation_data")
+    )
+    df_test = read_dataframe(
+        os.path.join(raw_data_path, f"{dataset}_testing_data")
+    )
+
+    # extract the target
+    target = 'full_dose'
+    y_train = df_train[target].values
+    y_valid = df_valid[target].values
+    y_test = df_test[target].values
+
+    # fit the dictvectorizer and preprocess data
+    dv = DictVectorizer()
+    X_train, dv = preprocess(df_train, dv, fit_dv=True)
+    X_valid, _ = preprocess(df_valid, dv, fit_dv=False)
+    X_test, _ = preprocess(df_test, dv, fit_dv=False)
+
+    # create dest_path folder unless it already exists
+    os.makedirs(dest_path, exist_ok=True)
+
+    # save dictvectorizer and datasets
+    dump_pickle(dv, os.path.join(dest_path, "dv.pkl"))
+    dump_pickle((X_train, y_train), os.path.join(dest_path, "train.pkl"))
+    dump_pickle((X_valid, y_valid), os.path.join(dest_path, "valid.pkl"))
+    dump_pickle((X_test, y_test), os.path.join(dest_path, "test.pkl"))
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--raw_data_path",
+        help="the location where the raw data was saved"
+    )
+    parser.add_argument(
+        "--dest_path",
+        help="the location where the resulting files will be saved."
+    )
+    args = parser.parse_args()
+
+    run(args.raw_data_path, args.dest_path)
---
+++ Scripts/register_model.py
@@ -0,0 +1,92 @@
+import argparse
+import os
+import pickle
+
+import mlflow
+from hyperopt import hp, space_eval
+from hyperopt.pyll import scope
+from mlflow.entities import ViewType
+from mlflow.tracking import MlflowClient
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+HPO_EXPERIMENT_NAME = "xgboost-hyperopt"
+EXPERIMENT_NAME = "xgboost-best-models"
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_experiment(EXPERIMENT_NAME)
+mlflow.xgboost.autolog()
+
+SPACE = {
+    'max_depth': scope.int(hp.quniform('max_depth', 4, 100, 1)),
+    'learning_rate': hp.loguniform('learning_rate', -3, 0),
+    'reg_alpha': hp.loguniform('reg_alpha', -5, -1),
+    'reg_lambda': hp.loguniform('reg_lambda', -6, -1),
+    'min_child_weight': hp.loguniform('min_child_weight', -1, 3),
+    'objective': 'reg:linear',
+    'seed': 42
+}
+
+
+def load_pickle(filename):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+
+def train_and_log_model(data_path, params):
+    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+    X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
+    X_test, y_test = load_pickle(os.path.join(data_path, "test.pkl"))
+
+    with mlflow.start_run():
+        params = space_eval(SPACE, params)
+        rf = RandomForestRegressor(**params)
+        rf.fit(X_train, y_train)
+
+        # evaluate model on the validation and test sets
+        valid_rmse = mean_squared_error(y_valid, rf.predict(X_valid), squared=False)
+        mlflow.log_metric("valid_rmse", valid_rmse)
+        test_rmse = mean_squared_error(y_test, rf.predict(X_test), squared=False)
+        mlflow.log_metric("test_rmse", test_rmse)
+
+
+def run(data_path, log_top):
+
+    client = MlflowClient()
+
+    # retrieve the top_n model runs and log the models to MLflow
+    experiment = client.get_experiment_by_name(HPO_EXPERIMENT_NAME)
+    runs = client.search_runs(
+        experiment_ids=experiment.experiment_id,
+        run_view_type=ViewType.ACTIVE_ONLY,
+        max_results=log_top,
+        order_by=["metrics.rmse ASC"]
+    )
+    for run in runs:
+        train_and_log_model(data_path=data_path, params=run.data.params)
+
+    # select the model with the lowest test RMSE
+    experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
+    # best_run = client.search_runs( ...  )[0]
+
+    # register the best model
+    # mlflow.register_model( ... )
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data_path",
+        default="./output",
+        help="the location where the prepared data was saved."
+    )
+    parser.add_argument(
+        "--top_n",
+        default=5,
+        type=int,
+        help="the top 'top_n' models will be evaluated to decide which model to promote."
+    )
+    args = parser.parse_args()
+
+    run(args.data_path, args.top_n)
